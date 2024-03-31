4844c5d79784db921e95c2bb2f8e4ba51f218c61
Lynarouissi <130801934+Lynarouissi@users.noreply.github.com>
2024-03-31 22:39:37 +0100

    Initial commit

---
+++ src/main/java/com/example/mise_a_jour_montant/MiseJourMontantApplication.java
@@ -0,0 +1,14 @@
+package com.example.mise_a_jour_montant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiseJourMontantApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MiseJourMontantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mise_a_jour_montant/MiseJourMontantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mise_a_jour_montant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiseJourMontantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
